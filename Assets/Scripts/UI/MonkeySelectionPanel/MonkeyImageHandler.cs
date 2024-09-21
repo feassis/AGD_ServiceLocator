8f823727c6b86df34cbcf56c72059750c4d6f51a
Felipe Assis <felipe.assis.2002@gmail.com>
2024-09-21 17:57:58 -0300

    Spawning monkey at end drag pos

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour, IDragHandler
+    public class MonkeyImageHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
         private RectTransform rectTransform;
+        private Vector3 originalPos;
+        private Vector3 originalAnchoredPos;
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -25,6 +27,8 @@ namespace ServiceLocator.UI
             monkeyImage.sprite = spriteToSet;
 
             rectTransform = GetComponent<RectTransform>();
+            originalPos = rectTransform.position; ;
+            originalAnchoredPos = rectTransform.anchoredPosition;
         }
 
         public void OnDrag(PointerEventData eventData)
@@ -33,6 +37,26 @@ namespace ServiceLocator.UI
 
             rectTransform.anchoredPosition += eventData.delta / UIService.Instance.rectTransform.localScale;
             owner.MonkeyDraggedAt(rectTransform.position);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            owner.MonkeyDroppedAt(eventData.position);
+            ResetMonkey();
+        }
+
+        private void ResetMonkey()
+        {
+            rectTransform.position = originalPos;
+            rectTransform.anchoredPosition = originalAnchoredPos;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
+            monkeyImage.color = new Color(1, 1, 1, 1f);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            monkeyImage.color = new Color(1, 1, 1, 0.6f);
         }
     }
 }
